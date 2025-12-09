7956e44d45e6db880eea1b92fb567ff118ab6a9c
sunjay77-bit <sanjaylamaa77@gmail.com>
2025-12-09 15:25:01 +0545

    data types

---
+++ 2.Datatypes.js
@@ -0,0 +1,29 @@
+"use script"; //treat all JS code as a newer version
+
+// alert("3+3")  We are using node JS not Browser
+
+console.log(3
+    +
+    3) // Code readablity should be high
+
+console.log("sunjay")
+
+
+let name = "sunjay"
+let age = "20"
+let loggedin = false
+let state = null
+
+// number = 2 to power 53
+//bignt
+//string = ""
+//boolean = true/false
+//null = standalone value
+//undefined = null
+//symbol = unique
+
+
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null); // object
