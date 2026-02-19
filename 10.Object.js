dc4f5ba022140ce337f452153c296d3c5d59f307
sunjay77-bit <sanjaylamaa77@gmail.com>
2026-02-19 20:01:30 +0545

    object frim chairaurcode

---
+++ 10.Object.js
@@ -0,0 +1,16 @@
+/** @format */
+
+// singleton
+
+// object literals
+const JsUser = {
+  name: "Sunjay",
+  age: 18,
+  location: "kathmandu",
+  email: "sunjay@gmail.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"],
+};
+
+console.log(JsUser["email"]);
+console.log(JsUser.age);
