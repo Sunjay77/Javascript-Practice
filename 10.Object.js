f0d419c8dd2e1ee510186897677dc91fd34d4a93
sunjay77-bit <sanjaylamaa77@gmail.com>
2026-02-19 20:30:45 +0545

    object

--- 10.Object.js
+++ 10.Object.js
@@ -3,14 +3,34 @@
 // singleton
 
 // object literals
+const mySym = Symbol("key1");
+
 const JsUser = {
   name: "Sunjay",
   age: 18,
+  [mySym]: "mykey1", // You have to use bracket [] to use syntax
   location: "kathmandu",
   email: "sunjay@gmail.com",
   isLoggedIn: false,
   lastLoginDays: ["Monday", "Saturday"],
 };
 
-console.log(JsUser["email"]);
-console.log(JsUser.age);
+console.log(JsUser["email"]); // You have to also use square bracket [] to log the string
+console.log(JsUser.age); // use dot notation to access object
+console.log(JsUser[mySym]);
+
+JsUser.age = 20;
+//Object.freeze(JsUser); Basicaly freeze the object after this any changes will not be applied
+JsUser.age = 10;
+console.log(JsUser);
+
+JsUser.greeting = function () {
+  console.log("Hello JS User");
+};
+
+JsUser.greetingTwo = function () {
+  console.log(`Hello JS User, ${this.name}`);
+};
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
