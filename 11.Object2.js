15b3a2c0b782c68bbb398b043f2b5a248d25f7b8
sunjay77-bit <sanjaylamaa77@gmail.com>
2026-02-21 12:28:54 +0545

    object notes

---
+++ 11.Object2.js
@@ -0,0 +1,53 @@
+/** @format */
+// const tinderUser = new Object()
+const tinderUser = {};
+tinderUser.id = "123abc";
+tinderUser.name = "Sammy";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+  email: "sun@gmail.com",
+  userfullname: {
+    firstname: "sunjay",
+    lastname: "lama",
+  },
+};
+
+// console.log(regularUser.userfullname);
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+
+// const obj3 = Object.assign({}, obj1, obj2); // giving empty paranthesis is not important but good to give
+
+// const obj3 = { ...obj1, ...obj2 };
+// console.log(obj3);
+
+const users = [
+  {
+    id: 1,
+    email: "sunjay@gmail.com",
+  },
+  {
+    id: 1,
+    email: "sunjay@gmail.com",
+  },
+  {
+    id: 1,
+    email: "sunjay@gmail.com",
+  },
+  {
+    id: 1,
+    email: "sunjay@gmail.com",
+  },
+];
+users[1].email;
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
