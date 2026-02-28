ed87eaec7b4f7f3710e9b2bc059cd292a35d29ee
sunjay77-bit <sanjaylamaa77@gmail.com>
2026-02-28 12:08:41 +0545

    scope

--- 14.scopes.js
+++ 14.scopes.js
@@ -9,13 +9,47 @@ if (true) {
   let a = 10;
   const b = 20;
   var c = 30;
-  console.log("INNER: ", a);
+  // console.log("INNER: ", a);
 }
 
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
-}
+// // for (let index = 0; index < array.length; index++) {
+//   // const element = array[index];
+// }
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+function one() {
+  const username = "sunjay";
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  // console.log(website);
+  two();
+}
+// one();
+
+if (true) {
+  const username = "sunjay";
+  if (username === "sunjay") {
+    const website = "youtube";
+    // console.log(username + website);
+  }
+  // console.log(website);
+}
+// console.log(username);
+
+// ++++++++ Interesting ++++++++
+
+function addone(num) {
+  return num + 1;
+}
+
+console.log(addone(5));
+
+// addTwo(5);
+const addTwo = function (num) {
+  return num + 2;
+};
